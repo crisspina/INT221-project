7a79e10f6b598d17d3e9a30bf596cde87a28951e
Pusit20574 <pusit.20574@mail.kmutt.ac.th>
2021-05-11 02:57:03 +0700

    [UPDATE] sql file

--- init.sql
+++ init.sql
@@ -1,12 +1,6 @@
 CREATE USER 'user'@'%' IDENTIFIED BY 'password' ;
 
-GRANT select, insert, update, delete on products to 'user'@'%';
-
-GRANT select, insert, update, delete on productdetails to 'user'@'%';
-
-GRANT select on colors to 'user'@'%';
-
-GRANT select on brands to 'user'@'%';
+GRANT ALL ON . TO 'user'@'%';
 
 CREATE TABLE brands (
     brandid    INTEGER NOT NULL,
@@ -30,7 +24,6 @@ CREATE TABLE productdetails (
 
 ALTER TABLE productdetails ADD CONSTRAINT productdetails_pk PRIMARY KEY ( productid,
                                                                           colorid );
-
 CREATE TABLE products (
     productid    INTEGER NOT NULL,
     modelname    VARCHAR(40) NOT NULL,
@@ -59,27 +52,7 @@ ALTER TABLE products
     ADD CONSTRAINT products_brands_fk FOREIGN KEY ( brandid )
         REFERENCES brands ( brandid );
 
-
-
 INSERT INTO brands ( brandid,brandname) VALUES ( 1,'casecoolors');
-
-
-INSERT INTO products
-VALUES(1,'iPhone12 ProMax','Rifle Green Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,1000 ,'2021-01-01','RifleGreenCase.jpg',1);
-INSERT INTO products
-VALUES(2,'iPhone12 ProMax','Madder Lake Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,1500 ,'2021-01-10','MadderLake.jpg',1);
-INSERT INTO products
-VALUES(3,'iPhone12 ProMax','Bittersweet Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,1800 ,'2021-03-20','Bittersweet.jpg',1);
-INSERT INTO products
-VALUES(4,'iPhone12 ProMax','Davys Grey Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,2200 ,'2021-03-26','DavysGreyCase.jpg',1);
-INSERT INTO products
-VALUES(5,'iPhone12 ProMax','Deep Space Sparkle Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,2200 ,'2021-04-03','DeepSpaceSparkleV2.jpg',1);
-INSERT INTO products
-VALUES(6,'iPhone12 ProMax','Alabaster Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,1600 ,'2021-04-10','Alabaster.jpg',1);
-INSERT INTO products
-VALUES(7,'iPhone12 ProMax','Tea Green Case','DEFENSiFY antimicrobial coating eliminates 99% of bacteria',50.00,1200 ,'2021-04-18','TeaGreen.jpg',1);
-
-
 
 INSERT INTO colors VALUES(1,'Deep Space Sparkle','#475D68');
 INSERT INTO colors VALUES(2,'Madder Lake','#C3343A');
@@ -88,7 +61,6 @@ INSERT INTO colors VALUES(4,'Davys Grey','#474747');
 INSERT INTO colors VALUES(5,'Rifle Green','#444C3F');
 INSERT INTO colors VALUES(6,'Alabaster','#F2EEE4');
 INSERT INTO colors VALUES(7,'Tea Green','#C4E2BE');
-
 
 INSERT INTO productdetails VALUES(1,1);
 INSERT INTO productdetails VALUES(2,2);
